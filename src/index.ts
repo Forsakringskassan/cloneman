efb6573676ffdc06540fbd07d5b6d2293944eaa7
Olof Fredriksson <oloflolof@gmail.com>
2026-03-31 15:07:51 +0200

    feat: export pack and publish function

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,5 @@ export {
 export { readJsonFile, readPackageJson } from "./utils";
 export { type BuildTemplateResult, buildTemplate } from "./template";
 export { type PackageJson } from "./utils/package-json";
+export { pack } from "./pack";
+export { publish } from "./publish";
--- src/pack.ts
+++ src/pack.ts
@@ -12,8 +12,10 @@ function getPackOutput(stdout: string): string {
 }
 
 /**
- * @internal
  * Packs the prepared template into a tarball using `npm pack` and moves it to the specified target directory.
+ *
+ * @public
+ * @since %version%
  * @param options - The options for packing the template.
  *   - `cwd`: The current working directory where the tarball should be moved after packing.
  *   - `targetDir`: The directory where the `npm pack` command should be executed.
