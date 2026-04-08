4a67c8cf26ce8c9a75cbe073c6aacf3b695acdfb
Olof Fredriksson <oloflolof@gmail.com>
2026-04-08 07:28:13 +0200

    feat: export prepare function
    to be used by the semantic release plugin.

--- src/prepare.ts
+++ src/prepare.ts
@@ -4,8 +4,16 @@ import spawn from "nano-spawn";
 import { isTemplateFolder } from "./utils/is-template";
 
 /**
- * @internal
+ * Prepares the target directory with a cloneman template by running the build script from the template's ".cloneman" folder.
+ * This target directory could then by used for packing the template with `npm pack` or publish.
+ *
+ * @public
+ * @since %version%
+ * @param options - The options for packing the template.
+ *   - `cwd`: The current working directory where the tarball should be moved after packing.
+ *   - `targetDir`: The directory where the `npm pack` command should be executed.
  */
+
 export async function prepare(cwd: string, targetDir: string): Promise<void> {
     if (!isTemplateFolder(cwd)) {
         throw new Error(
