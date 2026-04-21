1ce616d8bc45977c964e6ec13a466b2292320cce
David Sveningsson <david.sveningsson@forsakringskassan.se>
2026-04-21 12:35:28 +0200

    refactor: make `normalizeTemplateConfig()` idempotent

--- src/config/normalize-template-config.ts
+++ src/config/normalize-template-config.ts
@@ -4,14 +4,23 @@ import {
     normalizedConfig,
 } from "./template-config";
 
+function isNormalized(
+    config: TemplateConfig | NormalizedTemplateConfig,
+): config is NormalizedTemplateConfig {
+    return normalizedConfig in config;
+}
+
 /**
  * Normalize a template configuration file.
  *
  * @internal
  */
 export function normalizeTemplateConfig(
-    config: TemplateConfig,
+    config: TemplateConfig | NormalizedTemplateConfig,
 ): NormalizedTemplateConfig {
+    if (isNormalized(config)) {
+        return config;
+    }
     return {
         [normalizedConfig]: true,
         managedFiles: config.managedFiles ?? [],
